88a34f707b907dbeccd332cdbeff17d597c2c785
carrot <zakotoeichan@gmail.com>
2022-02-03 18:42:22 +0900

    add news and missing footer XD

--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,23 @@ $(function(){
     parent: $("#contents")
   });
 });
+
+
+
+jQuery(document).ready(function($){
+  $('ul.cards').on('click', function(){
+    $(this).toggleClass('transition');
+  });
+  
+  $('ul.card-stacks').on('click', function(){
+    $(this).toggleClass('transition');
+  });
+  
+  $('ul.cards-split').on('click', function(){
+    $(this).toggleClass('transition');
+  });
+  
+  $('ul.cards-split-delay').on('click', function(){
+    $(this).toggleClass('transition');
+  });
+});
